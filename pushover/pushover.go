4173626a9b9da8a16d0deb51d651bc18d12a6481
Nicholas Albright <ec2-user@ip-172-31-44-0.us-east-2.compute.internal>
2018-02-13 04:01:32 +0000

    Pushover Notifications

--- pushover/pushover.go
+++ pushover/pushover.go
@@ -1,4 +1,24 @@
-package pushover
-/* Place holder */
-func main(){}
+package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+const url = "https://api.pushover.net/1/messages.json"
+
+func Send(token string, user string, message string) (resp bool) {
+	resp = true
+	data := make(map[string]string)
+	data["token"] = token
+	data["user"] = user
+	data["message"] = message
+	mkjson, _ := json.Marshal(data)
+	_, err := http.Post(url, "application/json", bytes.NewBuffer(mkjson))
+	if err != nil {
+		resp = false
+	}
+	return
+
+}
